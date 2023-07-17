5d584734e6076b6e596d4b4771bfab2a22bd21af
docker <docker@docker.myguest.virtualbox.org>
2023-07-17 21:07:20 +0700

    ADD std structure function

--- src/parsing/Parsing.hpp
+++ src/parsing/Parsing.hpp
@@ -6,7 +6,26 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 20:56:33 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/07/16 20:56:34 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/07/17 21:04:18 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PARSING_HPP
+# define PARSING_HPP
+
+# include "../stdlib.hpp"
+
+class Parsing
+{
+    public:
+        Parsing();
+        Parsing(Parsing const & src);
+        ~Parsing();
+
+        Parsing &	operator=(Parsing const & rhs);
+
+    private:
+
+};
+
+#endif
--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -6,7 +6,24 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 20:56:31 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/07/16 20:56:32 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/07/17 21:05:06 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include "../stdlib.hpp"
+
+class Server
+{
+    public:
+        Server();
+        Server(Server const & src);
+        ~Server();
+
+        Server &	operator=(Server const & rhs);
+
+    private:
+
+};
