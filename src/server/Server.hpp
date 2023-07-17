183832d042f8738ce205bfa14562517ffeb0f654
Rati Chiewlian <rchiewli@z3t8c3.42bangkok.com>
2023-07-17 21:09:04 +0700

    simple init

--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -3,10 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rchiewli <rchiewli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 20:56:31 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/07/16 20:56:32 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:09:28 by rchiewli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef  SERVER_HPP
+#define  SERVER_HPP
+
+#include <exception>
+#include <ostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <streambuf>
+#include <unistd.h>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <queue>
+#include <ctime>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+class SimpleServer
+{
+    private:
+        const char *IndexHTML;
+        
+    protected:
+    public:
+    
+};
+
+#endif
