a3abf9835bda28b144c0a3051694501acaf315d6
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-08-18 18:10:40 +0700

    UPDATE add parsing function

--- src/parsing/Parsing.cpp
+++ src/parsing/Parsing.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parsing.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 20:56:34 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/07/17 21:06:16 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/08/18 18:03:46 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ Parsing::Parsing()
 
 }
 
+Parsing::Parsing(const std::string & file_path) {
+	this->_file.open(file_path.c_str(), std::ifstream::in);
+}
+
 Parsing::Parsing(Parsing const & src)
 {
-    *this = src;
+	*this = src;
 }
 
 Parsing::~Parsing()
@@ -29,9 +33,15 @@ Parsing::~Parsing()
 
 Parsing &	Parsing::operator=(Parsing const & rhs)
 {
-    if (this != &rhs)
-    {
-        //do something
-    }
-    return (*this);
+	if (this != &rhs)
+	{
+
+	}
+	return (*this);
+}
+
+void	Parsing::readFile(void) {
+	if (!this->_file.is_open())
+		throw CanNotOpenFile();
+
 }
--- src/parsing/Parsing.hpp
+++ src/parsing/Parsing.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parsing.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 20:56:33 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/07/17 21:04:18 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/08/18 18:00:48 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,22 @@
 
 class Parsing
 {
-    public:
-        Parsing();
-        Parsing(Parsing const & src);
-        ~Parsing();
+	private:
+		std::fstream	_file;
+	public:
+		Parsing();
+		Parsing(const std::string & file_path);
+		Parsing(Parsing const & src);
+		~Parsing();
 
-        Parsing &	operator=(Parsing const & rhs);
-
-    private:
+		Parsing &	operator=(Parsing const & rhs);
+		void	readFile(void);
+		class CanNotOpenFile: public std::exception {
+			public:
+				const char* what() const throw () {
+					return "Can not open file";
+				};
+		};
 
 };
 
