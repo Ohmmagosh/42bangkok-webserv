157e7d7a6d51b21e58a92b50212a21da29b3234e
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-08-18 20:04:07 +0700

    ADD HttpRequestHandle file first initial

---
+++ src/server/HttpRequestHandle.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpRequestHandle.cpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 18:12:46 by psuanpro          #+#    #+#             */
+/*   Updated: 2023/08/18 20:03:18 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HttpRequestHandle.hpp"
+
+HttpRequestHandle::HttpRequestHandle() {
+
+}
+
+HttpRequestHandle::HttpRequestHandle(const HttpRequestHandle & rhs) {
+
+}
+
+HttpRequestHandle::~HttpRequestHandle() {
+
+}
+
+HttpRequestHandle& HttpRequestHandle::operator=(const HttpRequestHandle & rhs) {
+
+}
