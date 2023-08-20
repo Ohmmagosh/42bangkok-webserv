7760d3680db87038641dc72727d39cd2cbafbfc5
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-08-20 23:36:23 +0700

    UPDATE comment test function

--- main.cpp
+++ main.cpp
@@ -4,26 +4,26 @@
 
 int main()
 {
-	{
-		std::fstream	file("config.conf");
-		if (!file.is_open()) {
-			std::cerr << "Unable to open file\n";
-			return 1;
-		}
+	// {
+	// 	std::fstream	file("config.conf");
+	// 	if (!file.is_open()) {
+	// 		std::cerr << "Unable to open file\n";
+	// 		return 1;
+	// 	}
 
-		std::string line;
-		std::stringstream	ss;
-		std::string		ret;
-		while (std::getline(file, line)) { // Read lines until end of file
-			ss << line << std::endl; // Print each line
-			// You can add more code here to process each line as needed
-		}
+	// 	std::string line;
+	// 	std::stringstream	ss;
+	// 	std::string		ret;
+	// 	while (std::getline(file, line)) { // Read lines until end of file
+	// 		ss << line << std::endl; // Print each line
+	// 		// You can add more code here to process each line as needed
+	// 	}
 
-		ret = ss.str();
-		std::cout << ret;
-		file.close(); // Close the file
-		return 0;
-	}
+	// 	ret = ss.str();
+	// 	std::cout << ret;
+	// 	file.close(); // Close the file
+	// 	return 0;
+	// }
 	// Server server(8080);
 	// server.start();
 	// return 0;
