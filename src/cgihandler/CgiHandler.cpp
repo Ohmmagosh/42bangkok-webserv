7e3dd62636083803b5db4323e91c5417d47003c4
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-08-24 00:06:43 +0700

    UPDATE simple structure function run

--- src/cgihandler/CgiHandler.cpp
+++ src/cgihandler/CgiHandler.cpp
@@ -6,7 +6,36 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 22:01:20 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/08/23 22:01:20 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/08/23 23:55:51 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "CgiHandler.hpp"
+
+
+CgiHandler::CgiHandler() {
+
+}
+
+CgiHandler::~CgiHandler() {
+
+}
+
+void	CgiHandler::run() {
+	int		pipefd[2];
+	pid_t	pid;
+
+	if (pipe(pipefd) == -1) {
+		throw PipeFail();
+	}
+
+	pid = fork();
+	if (pid == 0) {
+		//execve cgi
+	} else {
+		wait(NULL);
+
+		//send response to client
+	}
+	return ;
+}
--- src/cgihandler/CgiHandler.hpp
+++ src/cgihandler/CgiHandler.hpp
@@ -6,13 +6,14 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 22:01:13 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/08/23 22:03:10 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/08/23 23:52:56 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGIHANDLER_HPP
 # define CGIHANDLER_HPP
 
+# include "../stdlib.hpp"
 
 class CgiHandler {
 	private:
@@ -20,6 +21,12 @@ class CgiHandler {
 	public:
 		CgiHandler(/* args */);
 		~CgiHandler();
+		void	run();
+	class PipeFail: public std::exception {
+		const char* what() const throw () {
+			return "Pipe : can not pipe";
+		};
+	};
 };
 
 
