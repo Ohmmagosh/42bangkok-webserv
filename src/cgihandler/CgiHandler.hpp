2c524e2d50fdf5629324d627a10444d4837d9927
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-08-23 22:21:53 +0700

    ADD init src

---
+++ src/cgihandler/CgiHandler.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiHandler.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 22:01:13 by psuanpro          #+#    #+#             */
+/*   Updated: 2023/08/23 22:03:10 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGIHANDLER_HPP
+# define CGIHANDLER_HPP
+
+
+class CgiHandler {
+	private:
+		/* data */
+	public:
+		CgiHandler(/* args */);
+		~CgiHandler();
+};
+
+
+#endif
