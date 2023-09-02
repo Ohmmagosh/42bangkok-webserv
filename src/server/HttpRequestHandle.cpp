d702de71ff019854cc676f73e3b0f6810ad93888
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-09-02 19:31:54 +0700

    ADD structure request handle

--- src/server/HttpRequestHandle.cpp
+++ src/server/HttpRequestHandle.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpRequestHandle.cpp                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rchiewli <rchiewli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 18:12:46 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/08/29 16:02:27 by rchiewli         ###   ########.fr       */
+/*   Updated: 2023/09/02 19:21:08 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,35 @@ HttpRequestHandle::HttpRequestHandle() {
 
 }
 
-HttpRequestHandle::HttpRequestHandle(const HttpRequestHandle & rhs) {
-    (void)rhs;
+HttpRequestHandle::HttpRequestHandle( const Request & request) {
+	int	method_num;
+
+	method_num = this->mapRequestToMethod(request.getMethod());
+	switch (method_num) {
+		case 0:
+			//something
+			break;
+		case 1:
+			//something
+			break;
+		case 2:
+			//something
+			break;
+		case 3:
+			//something
+			break;
+		case 4:
+			//something
+			break;
+	}
+}
+
+Method	HttpRequestHandle::mapRequestToMethod(const std::string & method) {
+	if (method == "GET") return GET;
+	if (method == "POST") return POST;
+	if (method == "PUT") return PUT;
+	if (method == "DELETE") return DELETE;
+	return UNKNOW;
 }
 
 HttpRequestHandle::~HttpRequestHandle() {
@@ -25,10 +52,10 @@ HttpRequestHandle::~HttpRequestHandle() {
 }
 
 HttpRequestHandle& HttpRequestHandle::operator=(const HttpRequestHandle & rhs) {
-    // if (this != &rhs) {  // Check for self-assignment
-    //     this->someMemberVariable = rhs.someMemberVariable;
-    //     // Copy other member variables
-    // }
-    (void)rhs;
-    return *this;
+	// if (this != &rhs) {  // Check for self-assignment
+	//     this->someMemberVariable = rhs.someMemberVariable;
+	//     // Copy other member variables
+	// }
+	(void)rhs;
+	return *this;
 }
--- src/server/HttpRequestHandle.hpp
+++ src/server/HttpRequestHandle.hpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 18:12:48 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/08/18 20:01:14 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/09/02 19:16:15 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 # define HTTPREQUESTHANDLE_HPP
 
 # include "../stdlib.hpp"
+# include "Request.hpp"
 
+enum Method {GET, POST, PUT, DELETE, UNKNOW};
 class HttpRequestHandle
 {
 	private:
 	public:
 		HttpRequestHandle();
-		HttpRequestHandle(const HttpRequestHandle & rhs);
+		HttpRequestHandle(const Request & request);
 		~HttpRequestHandle();
 
 		HttpRequestHandle &operator=(const HttpRequestHandle & rhs);
+		Method	mapRequestToMethod(const std::string & method);
+		void	getMethod();
+		void	postMethod();
+		void	putMethod();
+		void	deleteMethod();
 
 };
 
