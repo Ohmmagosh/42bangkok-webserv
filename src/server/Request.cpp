eae8802eb114c1fcda08d21d7a72a86dcd1ed44b
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-10-11 23:51:14 +0700

    feat: update handler request

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 00:23:37 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/10/08 18:18:38 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/10/11 23:43:54 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,18 @@ Request::Request() {
 
 Request::Request(const std::string& raw_request) {
 	std::stringstream	requestStream(raw_request);
-	requestStream >> this->_method >> this->_path >> this->_protocol;
 	std::string line;
-	std::getline(requestStream, line);
-	while (std::getline(requestStream, line)) {
-		if (line == "\r")
-			break;
-		size_t colon_p = line.find(':');
-		if (colon_p != std::string::npos) {
-			std::string key = line.substr(0, colon_p);
-			std::string value = line.substr(colon_p + 2, line.size() - colon_p - 3);
-			this->_keys.push_back(key);
-			this->_headers[key] = value;
-		}
-	}
-	while (std::getline(requestStream, line)) {
-		this->_body = line;
-	}
+
+	this->setRawReqToVector(raw_request);
+	// std::cout << "-------------------------------------" << std::endl;
+	// std::cout << "----------------SIZE-----------------" << std::endl;
+	// std::cout << this->getReqVectorSize() << std::endl;
+	// std::cout << "----------------SIZE-----------------" << std::endl;
+	// std::cout << "----------------VECTOR-----------------" << std::endl;
+	// std::cout << this->getVectorReq() << std::endl;
+	// std::cout << "----------------VECTOR-----------------" << std::endl;
+	// std::cout << "-------------------------------------" << std::endl;
+	this->setHeaderAndBody(this->_req);
 }
 
 Request::~Request() {
@@ -72,15 +67,101 @@ int	Request::getHeaderSize() const {
 	return this->_headers.size();
 }
 
+void	Request::setHeader(const std::string& header) {
+	std::string			line;
+	std::stringstream	ss(header);
+	if (this->isMethod(header)) {
+		ss >> this->_method >> this->_path >> this->_protocol;
+	}
+	while (std::getline(ss,line)) {
+		size_t colon_p = line.find(':');
+		if (colon_p != std::string::npos) {
+			std::string key = line.substr(0, colon_p);
+			std::string value = line.substr(colon_p + 2, line.size() - colon_p - 3);
+			this->_keys.push_back(key);
+			this->_headers[key] = value;
+		}
+	}
+}
+
+
+std::vector<std::string>	Request::splite(const std::string& req, const std::string& delim) {
+	std::vector<std::string>	ret;
+	size_t						start = 0;
+	size_t						end = req.find(delim);
+
+	while (end != std::string::npos) {
+		ret.push_back(req.substr(start , end - start));
+		start = end + delim.length();
+		end = req.find(delim, start);
+	}
+	ret.push_back(req.substr(start, end ));
+	return ret;
+
+}
+
+void	Request::setRawReqToVector(const std::string& raw_request) {
+	std::string		delim = "\r\n\r\n";
+	this->_req = this->splite(raw_request, delim);
+}
+
+int	Request::getReqVectorSize() const{
+	return (this->_req.size());
+}
+
+const std::vector<std::string>&	Request::getVectorReq() const {
+	return (this->_req);
+}
 
 std::ostream& operator<<(std::ostream& os, const Request& req) {
-	os << "Protocol : " << req.getProtocol() << std::endl;
-	os << "Method : " << req.getMethod() << std::endl;
+	if (!req.getProtocol().empty())
+		os << "Protocol : " << req.getProtocol() << std::endl;
+	if (!req.getMethod().empty()) {
+		os << "Method : " << req.getMethod() << std::endl;
+	}
 	std::map<std::string, std::string>::const_iterator it = req.getMapHeader().begin();
 	while (it != req.getMapHeader().end()) {
 		os << it->first << " : " << it->second << std::endl;
 		it++;
 	}
+	std::cout << "-----------body----------" << std::endl;
 	os << req.getBody() << std::endl;
 	return (os);
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& vec) {
+	std::vector<std::string>::const_iterator it;
+
+	for (it = vec.begin(); it != vec.end(); it++) {
+		os << "--------------------------fdsafdsaff-----------------------------\n";
+		os << *it << std::endl;
+		os << "--------------------------ffasdfdsaf-----------------------------\n";
+	}
+	return (os);
+}
+
+void	Request::setHeaderAndBody(const std::vector<std::string>& req) {
+	if (req.size() == 2) {
+		this->setHeader(req[0]);
+		this->setBody(req[1]);
+	}
+	else if (req.size() == 1) {
+		if (this->isMethod(req[0])) {
+			this->setHeader(req[0]);
+		}
+	}
+}
+
+bool	Request::isMethod(const std::string& req_header) {
+	size_t	get = req_header.find("GET");
+	size_t	post = req_header.find("POST");
+	size_t	del = req_header.find("DELETE");
+
+	if (get != std::string::npos || post != std::string::npos || del != std::string::npos)
+		return true;
+	return false;
+}
+
+void	Request::setBody(const std::string& body) {
+	this->_body = body;
 }
--- src/server/Request.hpp
+++ src/server/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 00:17:34 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/10/05 13:10:44 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/10/11 23:04:44 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define REQUEST_HPP
 
 # include "../stdlib.hpp"
+
 
 class Request
 {
@@ -24,18 +25,29 @@ class Request
 		std::map<std::string, std::string>	_headers;
 		std::vector<std::string>			_keys;
 		std::string							_body;
+		std::vector<std::string>			_req;
 	public:
 		Request();
 		Request(const std::string& raw_request);
 		~Request();
-		const std::string& getMethod() const;
-		const std::string& getPath() const;
-		const std::string& getProtocol() const;
-		const std::string& getHeaderValue(const std::string& key) const;
-		const std::string& getBody() const;
-		const std::vector<std::string>&	getKeysHeader() const;
-		int		getHeaderSize() const;
-		const std::map<std::string, std::string>& getMapHeader() const;
+
+		int											getHeaderSize() const;
+		const std::string&							getMethod() const;
+		const std::string&							getPath() const;
+		const std::string&							getProtocol() const;
+		const std::string&							getHeaderValue(const std::string& key) const;
+		const std::string&							getBody() const;
+		const std::vector<std::string>&				getKeysHeader() const;
+		const std::map<std::string, std::string>&	getMapHeader() const;
+		std::vector<std::string>					splite(const std::string& req, const std::string& delim);
+		int											getReqVectorSize()const;
+		const std::vector<std::string>&				getVectorReq() const;
+		void										setHeader(const std::string& header);
+		void										setRawReqToVector(const std::string& raw_request);
+		void										setHeaderAndBody(const std::vector<std::string>& req);
+		bool										isMethod(const std::string& req_header);
+		void										setBody(const std::string& body);
+
 
 	class HeaderNotFound: public std::exception {
 		private:
@@ -52,6 +64,6 @@ class Request
 	};
 
 };
-std::ostream& operator<<(std::ostream& os, const  Request &req);
-
+std::ostream& operator<<(std::ostream& os, const  Request& req);
+std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& vec);
 #endif
