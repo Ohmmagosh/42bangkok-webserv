8e5c9e582259cfc5dbc619bf5e950500125850a4
Phatcharaphol Suanprom <psuanpro@z3t6c2.42bangkok.com>
2023-10-14 01:19:23 +0700

    feat: add Store and FileHandle

--- src/server/Store.cpp
+++ src/server/Store.cpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 21:52:22 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/10/11 22:53:22 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/10/14 00:36:33 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,45 @@ Store::~Store() {
 
 }
 
-const std::string&	Store::getBoundary() const {
-	return this->_boundary;
+const FileHandle*	Store::getFileByName(const std::string& name) const {
+	for (int i = 0; i < this->_file.size(); i++) {
+		if (this->_file[i].getName() == name) {
+			return &this->_file[i];
+		}
+	}
+	return nullptr ;
 }
 
-void	Store::setBoundary(const std::string& bound) {
-	this->_boundary = bound;
+void	Store::addFile(const FileHandle& file) {
+	this->_file.push_back(file);
 }
+
+bool	Store::searchFileName(const std::string& name) const {
+	for (int i = 0; i < this->_file.size(); i++) {
+		if (this->_file[i].getName() == name) {
+			return true ;
+		}
+	}
+	return false ;
+}
+
+void	Store::delFileByName(const std::string& name) {
+	for (int i = 0; i < this->_file.size(); i++) {
+		if (this->_file[i].getName() == name) {
+			this->_file.erase(this->_file.begin() + i);
+			return ;
+		}
+	}
+	return ;
+}
+
+void	Store::updateContentFileByName(const std::string& name, const std::string& content) {
+	for (int i = 0; i < this->_file.size(); i++) {
+		if (this->_file[i].getName() == name) {
+			this->_file[i].setContentOrAdd(content);
+			return ;
+		}
+	}
+	return ;
+}
+
--- src/server/Store.hpp
+++ src/server/Store.hpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <psuanpro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 21:52:26 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/10/11 22:53:20 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/10/14 00:35:50 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 # define STORE_HPP
 
 # include "../stdlib.hpp"
+# include "FileHandle.hpp"
 
 class Store
 {
 	private:
-		std::string	_boundary;
+		std::vector<FileHandle>	_file;
 	public:
 		Store();
 		~Store();
-		const std::string&	getBoundary() const;
-		void			setBoundary(const std::string& bound);
+		const FileHandle*	getFileByName(const std::string& name) const;
+		bool				searchFileName(const std::string& name) const;
+		void				addFile(const FileHandle& file);
+		void				delFileByName(const std::string& name);
+		void				updateContentFileByName(const std::string& name, const std::string& content);
 };
 
 #endif
